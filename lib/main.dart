4fcbe35b5036dfa6e942cc01dc4498c6392f0b44
Rifki-abd <abdullahrifki27@gmail.com>
2025-06-26 12:23:10 +0700

    Theme Setup Done,Splash Screen on Going

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:teknologi_mobile_prily/controllers/theme_controller.dart';
+import 'package:teknologi_mobile_prily/utils/app_themes.dart';
+import 'package:teknologi_mobile_prily/view/splash_screen.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  Get.put(ThemeController());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    final themeController = Get.find<ThemeController>();
+    return GetMaterialApp(
+      title: 'Preloved Daily',
+      theme: AppThemes.light,
+      darkTheme: AppThemes.dark,
+      themeMode: themeController.theme,
+      defaultTransition: Transition.fade,
+      home: const SplashScreen(),
     );
   }
-}
-
+  }
